879192b855e4f80afe00d886d46b31d0b8ee928d
Soni Lamkaj <soni.lamkaj@icongroup.com>
2022-08-05 16:33:41 +0200

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
         <meta name="description" content="Car service booking by Bosch" />
         <meta name="keywords"
           content="auto, vervangen, accu, airco, beurt, banden, leeg, versnellingsbak, apk, branden, grote, kapot, kleine, motorblok, garage, geluid, niet, start, autolampjes, band, condensor, controleren, dashboard, kapotte, keuring, lampjes, lege, loopt, motor, nakijken, onderhoud, piepen, problemen, remmen, repareren, snel, uitlijnen, wisselen, autobedrijf, autogarage, motorproblemen" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <title>Auto</title>
       </Head>
       {/* Bosch Horizontal rainbow header */}
